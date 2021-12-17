9815ac399203c21d5a71cf921746e9d16797500c
Noah <noahmldavis@hotmail.com>
2021-12-17 14:09:34 +0000

    added Snake class, grid rect and draw methods

--- main.py
+++ main.py
@@ -14,10 +14,28 @@ GRID_WIDTH, GRID_HEIGHT = 18, 18
 GRID_SIZE = 50
 
 
+class Snake():
+  def __init__(self) -> None:
+    self.body = [(9, 9), (8, 9), (7, 9)]
+    self.direction = 0
+    self.speed = 1
+
+
 def create_grid(width, height):
   grid = [[WHITE for x in range(width)] for y in range(height)]
 
   return grid
+
+
+def get_grid_rect(grid_x, grid_y):
+  rect = pygame.Rect(grid_x * GRID_SIZE, grid_y * GRID_SIZE, GRID_SIZE, GRID_SIZE)
+
+  return rect
+
+
+def draw_snake(surface, snake: Snake):
+  for piece in snake.body:
+    pygame.draw.rect(surface, BLACK, get_grid_rect(piece[0], piece[1]))
 
 
 def draw_window():
