5c762dc834f10b1dfff63a378bc5fdc95019e6e9
Noah <noahmldavis@hotmail.com>
2021-12-17 13:41:18 +0000

    defined game loop

--- main.py
+++ main.py
@@ -8,3 +8,26 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 
+FPS = 60
+
+
+def draw_window():
+  WIN.blit(WHITE, (0, 0))
+
+  pygame.display.update()
+
+
+def main():
+  clock = pygame.time.Clock()
+  run = True
+
+  while run:
+    clock.tick(FPS)
+
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        run = False
+        pygame.quit()
+  
+if __name__ == "__main__":
+  main()
