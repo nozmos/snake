ac01aa163675d47d60e9f3388e7eeb54cbc5eec7
Noah <noahmldavis@hotmail.com>
2021-12-17 13:38:31 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+
+WIDTH, HEIGHT = 900, 900
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Snake")
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+
